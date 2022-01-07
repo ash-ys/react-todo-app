5d3abb2cdc5cfdf720db6f477ad0b27f78ea3703
Ashish <53848069+ash-ys@users.noreply.github.com>
2022-01-07 15:56:39 +0545

    ay

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
     <p>React Todo</p>

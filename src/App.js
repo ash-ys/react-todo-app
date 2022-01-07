1ee7b7eb773ed3db0bf0c7d091c40a869b2770c3
Ashish <53848069+ash-ys@users.noreply.github.com>
2022-01-07 17:04:31 +0545

    u

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
-
+const LOCAL_STORAGE_KEY = "react-todo-list-todos"
 function App() {
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const storageTodos =JSON.parse(LOCAL_STORAGE_KEY);
+ }, []);
+ if(storageTodos){
+setTodos(storageTodos);
+ }
+  useEffect(() => {
+     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
   function addTodo(todo){
     setTodos([todo, ...todos]);
   }
   return (
-    <div className="App">
+    <div className= "App">
+    <header className="App-header">
     <p>React Todo</p>
     <TodoForm addTodo = {addTodo} />
     <TodoList todos = {todos} />
+    </header>
     </div>
   );
 }
